c7b7913c2fb050c661a6a7ef00441002f409cb3f
jackysiupuichung <110562553+jackysiupuichung@users.noreply.github.com>
2024-02-03 14:52:27 +0000

    Update data_processing.py
    added code for topic modelling

--- ChEMBL_visualisation/data_processing.py
+++ ChEMBL_visualisation/data_processing.py
@@ -1,4 +1,7 @@
 import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from bertopic import BERTopic
+import re
 
 def load_data(filename):
     return pd.read_csv(filename)
@@ -44,6 +47,40 @@ def process_data(filename):
     #df_expanded.to_csv('processed_data2.csv', index=False)
     return df_expanded
 
+def topic_modeling_on_assay_descriptions_w_bert(assay_descriptions):
+    """
+    Hollistic view of the topic_modeling process using BERTopic:
+
+    Embedding documents: This step involves converting the assay descriptions into numerical representations called embeddings. Embeddings capture the semantic meaning of the text by mapping it to a high-dimensional vector space.
+
+    Reducing dimensionality of embeddings: After embedding the documents, the dimensionality of the embeddings is reduced to a lower-dimensional space. This is done to simplify the data and remove noise, while still preserving the important information.
+
+    Clustering reduced embeddings into topics: The reduced embeddings are then clustered together based on their similarity. This helps in grouping similar assay descriptions together and forming topics.
+
+    Tokenization of topics: Once the topics are formed, they are tokenized into individual words or phrases. Tokenization breaks down the topics into smaller units for further analysis.
+
+    Weight tokens: Each token within a topic is assigned a weight based on its importance or relevance within the topic. This helps in identifying the key terms or concepts within each topic.
+
+    Represent topics with one or multiple representations: Finally, the topics are represented using one or multiple representations. This can include summarizing the topics with a few representative words or phrases, or using more complex representations such as word clouds or visualizations.
+    """
+    vectorizer_model = CountVectorizer(stop_words="english")
+    topic_model = BERTopic(vectorizer_model=vectorizer_model)
+
+    topics, probabilities = topic_model.fit_transform(assay_descriptions)
+    return topics, probabilities
+
+
+def preprocessing(assay_df):
+    assay_df['descriptions_processed'] = \
+    assay_df['description'].map(lambda x: re.sub('[,\.!?]', '', x))
+    # Convert the titles to lowercase
+    assay_df['descriptions_processed'] = assay_df['descriptions_processed'].map(lambda x: x.lower())
+    return assay_df
+
+def topic_modeling_on_assay_descriptions_w_lda(assay_df):
+    
+    assay_df = preprocessing(assay_df)
+    return
 
 def main():
     # Define the path to your input data file
