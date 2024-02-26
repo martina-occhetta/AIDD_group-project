c499ccac97f8148a8e67a678812ae1e80f4ad742
Yu-Yuan Yang at marvin (AF-lab) <yu-yuan.yang@qmul.ac.uk>
2024-02-26 18:17:52 +0000

    add post processing for OT_disease to target

--- OT_disease_to_target/pd_process.py
+++ OT_disease_to_target/pd_process.py
@@ -2,4 +2,10 @@ def str2list(s: str) -> list:
     """
     Convert a string to a list of strings
     """
-    return s.replace("'", "").strip('][').split(', ')
+    return s.replace("'", "").strip("][").split(", ")
+
+
+def expand_by_column(df, old_column_name, new_column_name):
+    new_df = df.copy()
+    new_df[new_column_name] = df[old_column_name].apply(str2list)
+    return new_df.explode(new_column_name)
